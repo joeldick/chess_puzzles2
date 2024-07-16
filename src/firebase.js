f516bcbda09198eb3ab7cf7f0dd5b16143bfa036
Joel Cato <joeldic@gmail.com>
2024-07-16 17:05:55 -0400

    added Prettier for formatting

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB8Zki6WT7zv7OhibmhAgzKSg60HOGbrww",
-    authDomain: "chess-puzzles2.firebaseapp.com",
-    projectId: "chess-puzzles2",
-    storageBucket: "chess-puzzles2.appspot.com",
-    messagingSenderId: "237749960690",
-    appId: "1:237749960690:web:51820f98a2a650041fb56b",
-    measurementId: "G-N4HJWMTSS9"
+  apiKey: 'AIzaSyB8Zki6WT7zv7OhibmhAgzKSg60HOGbrww',
+  authDomain: 'chess-puzzles2.firebaseapp.com',
+  projectId: 'chess-puzzles2',
+  storageBucket: 'chess-puzzles2.appspot.com',
+  messagingSenderId: '237749960690',
+  appId: '1:237749960690:web:51820f98a2a650041fb56b',
+  measurementId: 'G-N4HJWMTSS9',
 };
 
 const app = initializeApp(firebaseConfig);
